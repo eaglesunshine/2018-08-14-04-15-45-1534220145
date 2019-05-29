db29e2dc12d60596c8ceace96c4e46695b6b2878
eaglesunshine <18900927316@163.com>
2019-05-29 23:11:51 +0800

    add code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,22 @@
 // Write your cade below:
-module.exports = function main() {
-  
-};
+	const readline = require('readline');  
+    const rl = readline.createInterface({  
+        input: process.stdin,  
+        output: process.stdout  
+    });  
+    var inputArr = [];  
+    rl.on('line', function (input) {  
+        inputArr = input.split(" ");  
+        inputArr.forEach(function(item,index){  
+            inputArr[index] = +item;// 转化为数字  
+        });  
+        // 下面就可以对数据进行处理......  
+        console.log(inputArr[0]%inputArr[1]);  
+        inputArr = [];// 清空数组  
+        rl.close();  
+    });  
+      
+    rl.on('close', function() {    
+        process.exit(0);  
+    });
+
